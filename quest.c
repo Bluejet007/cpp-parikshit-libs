d6b19e07c3593ac53e314abe39235d449068ad98
Bluejet007 <rachit.takate@gmail.com>
2025-05-31 12:09:30 +0530

    Added vital files

--- quest.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "vector3Math.h"
-
-void main() {
-    Matrix3 mat = {{{2, 4, 4}, {2, 1, 3}, {7, 9, 1}}};
-
-    printf("Det = %f\n", getMatDet(&mat));
-}
