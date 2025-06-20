abc95131cf2800b58e1cfb1be62f2e8070d71fe7
Bluejet007 <rachit.takate@gmail.com>
2025-06-20 17:50:49 +0530

    Added Vector3 to Matrix3 constructors

--- unifiedMath.hpp
+++ unifiedMath.hpp
@@ -12,9 +12,39 @@ const float radToDeg = 180.0f / M_PI;
 const float degToRad = 1.0f / radToDeg;
 
 
+// Constructors for Matrix3 using three Vector3s.
+// Missing parametres are zero filled.
+
+// Each vector forms a row.
+Matrix3 matrixFromRows(Vector3 rowA, Vector3 rowB = Vector3(), Vector3 rowC = Vector3()) {
+    float mat[3][3] = {};
+
+    for(char i = 0; i < 3; i++) {
+        mat[0][i] = rowA[i];
+        mat[1][i] = rowB[i];
+        mat[2][i] = rowC[i];
+    }
+
+    return Matrix3(mat);
+}
+
+// Each vector forms a column.
+Matrix3 matrixFromColumns(Vector3 colA, Vector3 colB = Vector3(), Vector3 colC = Vector3()) {
+    float mat[3][3] = {};
+
+    for(char i = 0; i < 3; i++) {
+        mat[i][0] = colA[i];
+        mat[i][1] = colB[i];
+        mat[i][2] = colC[i];
+    }
+
+    return Matrix3(mat);
+}
+
+
 // Multiplications between matrices and vectors
 // (matrices who have a dimension equal to 1)
-// automatically account the needed transpose
+// automatically account the needed transpose.
 
 // Vector * matrix
 Vector3 operator*(const Vector3& vec, const Matrix3& mat) {
